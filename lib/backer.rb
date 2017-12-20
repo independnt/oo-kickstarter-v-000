50e5acc641ff847b75ff1781876d99311ce25959
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-20 20:39:54 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,15 @@
 class Backer
-
+backed_projects = []
   attr_accessor :name, :project
 
 def initialize(name)
   @name = name
 end
+
+def back_project(project)
+  back_projects << project
+end
+
 
 
 end
